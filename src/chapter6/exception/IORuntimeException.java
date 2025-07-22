bcf8127598e6c095bffd5a34f9b8538034485bbe
shuji_koike <koike.shuji@alhinc.jp>
2025-07-22 14:48:18 +0900

    14. ログアウト機能を実装

---
+++ src/chapter6/exception/IORuntimeException.java
@@ -0,0 +1,13 @@
+//Exception：例外
+package chapter6.exception;
+
+import java.io.IOException;
+
+public class IORuntimeException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public IORuntimeException(IOException cause) {
+		super(cause);
+	}
+
+}
